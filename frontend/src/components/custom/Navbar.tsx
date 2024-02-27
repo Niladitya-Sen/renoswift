17abacaae9367307639abf45e40144fb6bc6106c
Niladitya-Sen <niladityasen.2212@gmail.com>
2024-02-28 00:02:08 +0530

    Update Navbar component to show MobileSidebar on 'ot' path

--- frontend/src/components/custom/Navbar.tsx
+++ frontend/src/components/custom/Navbar.tsx
@@ -123,7 +123,7 @@ export default function Navbar({ className }: Readonly<{ className?: string }>) 
                     </DropdownMenu>
                 </div>
                 {
-                    !pathname.includes('admin') && <MobileSidebar />
+                    pathname.includes('ot') && <MobileSidebar />
                 }
             </SectionWrapper>
         </nav>
