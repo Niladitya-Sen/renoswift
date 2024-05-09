a1a23d432bb84b71116390505eaf6e63a6c116f4
Niladitya Sen <niladityasen.2212@gmail.com>
2024-05-09 22:33:34 +0530

    chore: Remove devToken cookie on logout

--- frontend/src/lib/logout.ts
+++ frontend/src/lib/logout.ts
@@ -2,6 +2,7 @@ export default function logout() {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "adminToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "otToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "devToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
     if (window.location.pathname === "/") {
         window.location.reload();
