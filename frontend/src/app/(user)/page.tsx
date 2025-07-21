6ceb439e579aa1cbd29781359d9d95a492683553
Niladitya Sen <niladityasen.2212@gmail.com>
2025-07-21 13:19:54 +0530

    Refactor imports and clean up layout and login components for consistency

--- frontend/src/app/(user)/page.tsx
+++ frontend/src/app/(user)/page.tsx
@@ -1,32 +1,23 @@
-import AboutUs from '@/components/custom/AboutUs';
-import Categories from '@/components/custom/Categories';
-import ChatBot from '@/components/custom/ChatBot';
-import ChooseUs from '@/components/custom/ChooseUs';
-import ClientReview from '@/components/custom/ClientReview';
-import Hero from '@/components/custom/Hero';
-import Inspiration from '@/components/custom/Inspiration';
-import Newsletter from '@/components/custom/Newsletter';
-import SectionWrapper from '@/components/custom/SectionWrapper';
-import Working from '@/components/custom/Working';
-import { cn } from '@/lib/utils';
+import AboutUs from "@/components/custom/AboutUs";
+import Categories from "@/components/custom/Categories";
+import Hero from "@/components/custom/Hero";
+import Inspiration from "@/components/custom/Inspiration";
+import Newsletter from "@/components/custom/Newsletter";
+import SectionWrapper from "@/components/custom/SectionWrapper";
+import Working from "@/components/custom/Working";
+import { cn } from "@/lib/utils";
 
 export default function Index() {
-    return (
-        <>
-        
-            <Hero />
-            <SectionWrapper className={cn('w-full')}>
-                <AboutUs />
-                {/* <ChooseUs /> */}
-                <Categories />
-                {/* <Products /> */}
-                <Inspiration />
-                <Working />
-                <ClientReview />
-                <Newsletter />
-                {/* <ChatBot /> */}
-            </SectionWrapper>
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <SectionWrapper className={cn("w-full")}>
+        <AboutUs />
+        <Categories />
+        <Inspiration />
+        <Working />
+        <Newsletter />
+      </SectionWrapper>
+    </>
+  );
 }
-
--- frontend/src/app/auth/layout.tsx
+++ frontend/src/app/auth/layout.tsx
@@ -1,20 +1,11 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import React from "react";
 
-export default function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <section className='bg-[url("/renovate-pro/assets/auth/authBg.webp")] bg-cover bg-center bg-no-repeat h-screen px-2 flex items-center'>
-            <Link href='/'>
-                <Image
-                    src={'/assets/logo.jpg'}
-                    alt='logo'
-                    width={200}
-                    height={100}
-                    className='absolute top-2 right-2'
-                />
-            </Link>
-            {children}
-        </section>
-    )
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <section className='bg-[url("/renovate-pro/assets/auth/authBg.webp")] bg-cover bg-center bg-no-repeat h-screen px-2 flex items-center'>
+      {children}
+    </section>
+  );
 }
